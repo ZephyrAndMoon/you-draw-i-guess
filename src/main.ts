9a5c5f59e1c0f90a780dada19c7c57b062aa750e
zephyr <1024676057@qq.com>
2021-11-02 10:52:46 +0800

    chore: 集成 postcss && scss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "amfe-flexible/index.js";
+
+createApp(App).mount("#app");
