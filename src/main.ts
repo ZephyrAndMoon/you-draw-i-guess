6705ad35a200d3c67dd17eb3bcd04e81d416df94
zephyr <1024676057@qq.com>
2021-11-02 11:26:11 +0800

    chore: 引入varlet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// 引入 varlet UI
+import Varlet from "@varlet/ui";
+import "@varlet/ui/es/style.js";
+
 import "amfe-flexible/index.js";
 
-createApp(App).mount("#app");
+createApp(App).use(Varlet).mount("#app");
